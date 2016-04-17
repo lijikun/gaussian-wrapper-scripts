1d7af6417467326a6ac6d8d1ff55582b3d0ef517
Jikun Li <jikun.li@icloud.com>
2016-04-17 02:28:28 -0400

    Trying to defend against filenames with whitespace

--- g09template.sh
+++ g09template.sh
@@ -4,38 +4,38 @@
 # Usage: g09template.sh template molecule1 [molecule2 ...] [-n number-of-processors] [-s script-file] 
 
 # Default parameters:
-scriptName="g09batch.sh"
+scriptName=
 nProcs=1
 template=
 coords=
 scriptCommand="g09"
 
 # Processes command line arguments:
-while [[ $1 ]]; do
+while [[ "$1" ]]; do
     case "$1" in
         -s)
             if [[ "$2" ]]; then
-                scriptName=$2
+                scriptName="$2"
                 shift 2
             else
-                echo "Warning: No script file name specified. Default to ${scriptName}."
+                echo "Warning: No script file name specified."
                 shift
             fi;;
         -n)
             isNProcs='^0*[1-9][0-9]*$'
-            if [[ $2 && $2 =~ $isNProcs ]]; then
+            if [[ "$2" && "$2" =~ $isNProcs ]]; then
                 nProcs=$2
                 shift 2
             else
-                echo "Warning: Invalid number of processors $2. Default to ${nProcs}."
+                echo "Warning: Invalid number of processors $2."
                 shift
             fi;;
         *)
-            if [[ -f $1 ]]; then
-                if [[ $template ]]; then
-                    coords=${coords}$'\n'$1 
+            if [[ -f "$1" ]]; then
+                if [[ "$template" ]]; then
+                    coords="${coords}"$'\n'"$1" 
                 else
-                    template=$1
+                    template="$1"
                 fi
             else
                 echo "Warning: File $1 does not exist. Ignored."
@@ -44,50 +44,51 @@ while [[ $1 ]]; do
     esac
 done
 [[ $template ]] || { echo "Error: No template file specified."; exit 1; }
-[[ $coords ]] || { echo "Error: No coordinate file(s) specified."; exit 1; }
+[[ $coords ]] || { echo "Error: No molecule coordinate file(s) specified."; exit 1; }
+[[ $scriptName ]] || scriptName="${template}.sh"
 
 
 # Generates g09 input files:
 echo "#Processors = ${nProcs}"
 echo "Template file: ${template}"
 echo
+[[ -f $scriptName ]] && mv "$scriptName" "${scriptName}.bak" && "${scriptName} backed up."
+{ echo '#!/bin/bash'; echo; } > "$scriptName"
 echo "Generated input files:"
-[[ -f $scriptName ]] || { echo '#!/bin/bash'; echo; } > $scriptName
 OLDIFS=$IFS
 IFS=$'\n'
-for x in $coords;
-do
+for x in "$coords"; do
     extension='\.\w*$'
-    baseName=$(basename $x)
-    if [[ $baseName =~ $extension ]]; then
-        title=${baseName%${BASH_REMATCH[0]}}
+    baseName="$(basename $x)"
+    if [[ "$baseName" =~ $extension ]]; then
+        title="${baseName%${BASH_REMATCH[0]}}"
     else
-        title=$baseName
+        title="$baseName"
     fi
     inputFile="${title}_$(basename $template)"
-    { cat $template | while read templateLine; do
-        case $templateLine in
+    { cat "$template" | while read templateLine; do
+        case "$templateLine" in
             !chkfile!)
                 echo "%chk=${inputFile}.chk" ;;
             !nprocs!)
                 echo "%nprocshared=${nProcs}" ;;
             !title!)
-                echo ${inputFile} ;;
+                echo "${inputFile}" ;;
             !coordinates!)
                 [[ $discardLine ]] || discardLine='^[0-9]*$'
                 cat $x | while read coordLine; do
-                    [[ $coordLine =~ $discardLine ]] || echo ${coordLine}
+                    [[ $coordLine =~ $discardLine ]] || echo "${coordLine}"
                 done ;;
             *)
-                echo ${templateLine} ;;
+                echo "${templateLine}" ;;
         esac
-    done; } > $inputFile
+    done; } > "${inputFile}"
     # Adds an empty line to end of file if there isn't one in the template.
-    [[ $(tail -n1 $inputFile) ]] && echo >> $inputFile
-    echo "${scriptCommand} ${inputFile}" >> $scriptName
+    [[ $(tail -n1 "${inputFile}") ]] && echo >> "$inputFile"
+    echo "${scriptCommand} ${inputFile}" >> "$scriptName"
     echo "  ${inputFile}"
 done
 IFS=$OLDIFS
 echo
 echo "Generated run script: ${scriptName}"
-chmod +x $scriptName
+chmod +x "$scriptName"
