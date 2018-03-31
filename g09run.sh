d9e435aa24472907a84e95449467b2637c4ca2df
Jikun Li <jikun.li@icloud.com>
2018-03-31 11:11:36 -0400

    Customizable command name.

--- g09run.sh
+++ g09run.sh
@@ -4,25 +4,27 @@
 # Usage: g09run input-filename [email] 
 
 extension='.com$|.gjf$'
+exec_name='g09'
 
-if which g09
+if which $exec_name
 then
     if [[ -f "$1" ]]
     then
         if [[ "$1" =~ $extension ]]  # Regex for correct extensions of the files.
         then
-            echo "Running: g09 $1...."
+            echo "Running: $exec_name $1...."
             outputFile="${1/%${BASH_REMATCH[0]}/\.log}"
-            g09 "$1"
+            $exec_name "$1"
             if [[ "$2" ]] && which mail
             then
                 echo "Mailing output to $2...."
-                tail $outputFile | mail $2 -s "G09 Job $1 Result"                       
+                tail $outputFile | mail $2 -s "$exec_name Job $1 Result"                       
             else
-                echo "Email address not specified or no "mail" command available. Skipping mailing." >&2
+                echo 'Email address not specified or no "mail" command available. Skipping mailing.' >&2
             fi
         else
             echo "Input must have extension of .com or .gjf." >&2
+            echo "Usage: g09run.sh [job file name] [email address]"
             exit 2
         fi
     else
@@ -30,7 +32,7 @@ then
         exit 1
     fi
 else
-    echo "G09 is not initialized." >&2
+    echo "$exec_name is not initialized." >&2
     exit 255
 fi
 
